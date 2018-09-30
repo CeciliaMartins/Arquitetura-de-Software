f4f5ce8bb6aadf8bb16ecec03f2ed6b85d38ebe3
_Ciss <martins.ceciliaa@gmail.com>
2018-09-30 07:26:25 -0300

    Pipoca 02

--- Pipoca/pipoca/src/br/usjt/arqsw18/pipoca/controller/HelloWorldController.java
+++ Pipoca/pipoca/src/br/usjt/arqsw18/pipoca/controller/HelloWorldController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
-	
+
 	@RequestMapping("/hello")
-	public String execute(){
-	System.out.println("chamou o controller hello");
-	return "hello";
+	public String execute() {
+		System.out.println("chamou o controller hello");
+		return "hello";
 	}
 }
